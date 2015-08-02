7f1e3dcc2a10a9f7c31b15b4bfc2409cd9882242
pemrouz <p3pedram@hotmail.com>
2015-08-02 15:22:02 +0100

    add support for transforming parent data

--- test.js
+++ test.js
@@ -1,16 +1,21 @@
 var expect = require('chai').expect
+  , key = require('utilise.key')
   , inherit = require('./')
 
 describe('inherit', function() {
-  it('should single-valued array with parent data', function() {
+  it('should create single-valued array with parent data', function() {
     expect(inherit({ foo:'bar' }, 1)).to.eql([{ foo:'bar' }])
   })
 
-  it('should default single-valued array with parent data', function() {
+  it('should default to single-valued array with parent data', function() {
     expect(inherit()({ foo:'bar' }, 1)).to.eql([{ foo:'bar' }])
   })
 
-  it('should multi-valued array with parent data', function() {
+  it('should create multi-valued array with parent data', function() {
     expect(inherit(2)({ foo:'bar' }, 1)).to.eql([{ foo:'bar' }, { foo:'bar' }])
+  })
+
+  it('should create single-valued array with transformed parent data', function() {
+    expect(inherit(key('foo'))({ foo: 'bar' }, 1)).to.eql(['bar'])
   })
 })
